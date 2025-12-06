dd0ade29540df19502a9c2c72b713762100e68e9
devhouse4670 <devhouse4670@gmail.com>
2025-12-06 16:01:29 +0400

    Update server.js
    remove head

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie');
+
   
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
